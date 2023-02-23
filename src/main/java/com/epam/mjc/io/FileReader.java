9184067bb9116592c8da6f18f5f2e6824a6e0f4d
epampam <epampam@wp.pl>
2023-02-23 18:02:09 +0100

    finished

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,36 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            int ch;
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((ch = fileInputStream.read()) != -1) {
+                stringBuilder.append((char) ch);
+            }
+            String[] lines = stringBuilder.toString().split("\\n");
+            for (int i = 0; i < lines.length; i++) {
+                if (i % 4 == 0) {
+                    profile.setName(lines[i].substring(6).replace("\r", ""));
+                } else if (i % 4 == 1) {
+                    profile.setAge(Integer.valueOf(lines[i].substring(5).replace("\r", "")));
+
+                } else if (i % 4 == 2) {
+                    profile.setEmail(lines[i].substring(7).replace("\r", ""));
+
+                } else {
+                    profile.setPhone(Long.valueOf(lines[i].substring(7).replace("\r", "")));
+                }
+
+            }
+        } catch (IOException e) {
+// exception handling
+        }
+        return profile;
     }
 }
