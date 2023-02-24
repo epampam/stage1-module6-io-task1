a23539be0844458c15c2257f8290065a5a27e602
epampam <epampam@wp.pl>
2023-02-24 20:56:24 +0100

    back to the first version

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -2,23 +2,34 @@ package com.epam.mjc.io;
 
 import java.io.*;
 
+
 public class FileReader {
+
     public Profile getDataFromFile(File file) {
         Profile profile = new Profile();
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
-            profile.setName(reader.readLine().substring(6).trim());
-            profile.setAge(Integer.parseInt(reader.readLine().substring(5).trim()));
-            profile.setEmail(reader.readLine().substring(7).trim());
-            profile.setPhone(Long.parseLong(reader.readLine().substring(7).trim()));
-        } catch (FileNotFoundException e) {
-            System.err.println("File not found: " + file.getAbsolutePath());
-            return null;
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            int ch;
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((ch = fileInputStream.read()) != -1) {
+                stringBuilder.append((char) ch);
+            }
+            String[] lines = stringBuilder.toString().split("\\n");
+            for (int i = 0; i < lines.length; i++) {
+                if (i % 4 == 0) {
+                    profile.setName(lines[i].substring(6).replace("\r", ""));
+                } else if (i % 4 == 1) {
+                    profile.setAge(Integer.valueOf(lines[i].substring(5).replace("\r", "")));
+
+                } else if (i % 4 == 2) {
+                    profile.setEmail(lines[i].substring(7).replace("\r", ""));
+
+                } else {
+                    profile.setPhone(Long.valueOf(lines[i].substring(7).replace("\r", "")));
+                }
+
+            }
         } catch (IOException e) {
-            System.err.println("Error reading file: " + e.getMessage());
-            return null;
-        } catch (Exception e) {
-            System.err.println("Unexpected error: " + e.getMessage());
-            return null;
+// exception handling
         }
         return profile;
     }
